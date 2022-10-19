67e16f7cb44c6cc2a2f28e0b3c4e29f2eb2aea82
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2022-10-19 15:55:48 -0300

    Ejercicio_04 terminado

---
+++ Ejercicio_04/main.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+struct Nodo
+{
+    int info;
+    Nodo *sgte;
+};
+
+void insertarOrdenado(Nodo *&lista, int ainsertar)
+{
+    Nodo *paux = lista;
+    Nodo *anterior = NULL;
+    while (paux && paux->info < ainsertar)
+    {
+        anterior = paux;
+        paux = paux->sgte;
+    }
+    if (paux != lista)
+    {
+        anterior->sgte = new Nodo();
+        anterior->sgte->info = ainsertar;
+        anterior->sgte->sgte = paux;
+    }
+    else
+    {
+        lista = new Nodo();
+        lista->sgte = paux;
+        lista->info = ainsertar;
+    }
+    return;
+}
+
+void MostrarLista(Nodo *milista)
+{
+    while (milista)
+    {
+        cout << milista->info << "   " << milista << endl;
+        milista = milista->sgte;
+    }
+}
+
+void agregarNodo(Nodo *&p, int x)
+{
+    Nodo *nuevo = new Nodo();
+    nuevo->info = x;
+    nuevo->sgte = NULL;
+
+    if (p == NULL)
+    {
+        p = nuevo;
+    }
+    else
+    {
+
+        Nodo *aux = p;
+
+        while (aux->sgte != NULL)
+        {
+            aux = aux->sgte;
+        }
+
+        aux->sgte = nuevo;
+    }
+
+    return;
+}
+
+void unirListas(Nodo *listaA, Nodo *listaB, Nodo *&listaC)
+{
+
+    while (listaA != NULL)
+    {
+        agregarNodo(listaC, listaA->info);
+        listaA = listaA->sgte;
+    }
+
+    while (listaB != NULL)
+    {
+        agregarNodo(listaC, listaB->info);
+        listaB = listaB->sgte;
+    }
+}
+
+void ordenarLista(Nodo *&lista)
+{
+    Nodo *paux = lista;
+    Nodo *siguiente = NULL;
+    int aux;
+    while (paux)
+    {
+        siguiente = paux->sgte;
+        while (siguiente)
+        {
+            if (paux->info > siguiente->info)
+            {
+                aux = paux->info;
+                paux->info = siguiente->info;
+                siguiente->info = aux;
+            }
+            siguiente = siguiente->sgte;
+        }
+        paux = paux->sgte;
+    }
+}
+
+int main()
+{
+
+    Nodo *listaA = NULL;
+    insertarOrdenado(listaA, 5);
+    insertarOrdenado(listaA, 90);
+    insertarOrdenado(listaA, 65);
+    insertarOrdenado(listaA, 24);
+
+    Nodo *listaB = NULL;
+
+    insertarOrdenado(listaB, 1);
+    insertarOrdenado(listaB, 12);
+    insertarOrdenado(listaB, 76);
+    insertarOrdenado(listaB, 100);
+
+    cout << "LISTA A: " << endl;
+    MostrarLista(listaA);
+    cout << "--------------------------" << endl;
+
+    cout << "LISTA B: " << endl;
+    MostrarLista(listaB);
+    cout << "--------------------------" << endl;
+
+    Nodo *listaC = NULL;
+    unirListas(listaA, listaB, listaC);
+
+    Nodo *aux = listaC;
+
+    ordenarLista(aux);
+
+    while (aux)
+    {
+        cout << aux->info << endl;
+        aux = aux->sgte;
+    }
+
+    return 0;
+}
